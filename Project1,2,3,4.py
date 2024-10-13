623c50f4110b42e2a035869c65a2b9a328cbfdf9
Mohamadrhp <132127164+Mohamadrhp@users.noreply.github.com>
2024-10-13 17:10:36 +0330

    Add files via upload

---
+++ Project1,2,3,4.py
@@ -0,0 +1,23 @@
+#Project1:
+name = "Mohamad mahdi"
+family = "Rahimpour"
+FullName = name + family
+print(FullName)
+
+#Project2:
+NumberOne = 5
+NumberTwo = 16
+Sum = NumberOne + NumberTwo
+print(Sum)
+
+#Project3:
+a = input("Please enter your first number")
+b = input("Please enter your second number")
+c = a + b
+print(c)
+
+#Project4:
+Aval = int(input("Please enter your first number"))
+Dovom = int(input("Please enter your second number"))
+jam = Aval + Dovom
+print(jam)
